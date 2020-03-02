5c228b67b67810eaf18c458cafb1888b9478ce30
wen <alejet.larvata@gmail.com>
2020-03-02 17:08:53 +0800

    feat 建立公司與公司組織跟節點service

--- lib/org_ps/service/org_nodes.rb
+++ lib/org_ps/service/org_nodes.rb
@@ -1,6 +1,17 @@
 module OrgPs
   module Service
     class OrgNodes
+      def create_company_node(gui_number, name, office_address)
+        company = OrgPs::Factory::Orgs.create_company(gui_number, name, office_address)
+        root = OrgPs::Factory::Nodes.create_root(company)
+        {
+          organization: company,
+          tree_node: root
+        }
+      end
+
+      def add_org_node()
+      end
     end
   end
 end
