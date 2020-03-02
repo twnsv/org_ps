0771fedaa6c0b06414fd59e36aabccc5fdc37e85
wen <alejet.larvata@gmail.com>
2020-03-02 16:34:31 +0800

    feat 建立公司組織根節點

--- lib/org_ps/factory/nodes.rb
+++ lib/org_ps/factory/nodes.rb
@@ -1,6 +1,10 @@
 module OrgPs
   module Factory
     class Nodes
+      def self.create_root(nodeable)
+        org = nodeable[:nodeable]
+        TreeNode.company.enabled.create(nodeable: org, org.gui_number, name: org.abbrev_name)
+      end
     end
   end
 end
