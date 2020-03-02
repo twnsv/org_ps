73b8437db3cf96037d444bc27a5da43e767e377e
wen <alejet.larvata@gmail.com>
2020-03-02 17:13:57 +0800

    feat 建立組織與節點的service方法改為靜態方法

--- lib/org_ps/service/org_nodes.rb
+++ lib/org_ps/service/org_nodes.rb
@@ -1,22 +1,24 @@
 module OrgPs
   module Service
     class OrgNodes
-      def create_company_node(gui_number, name, office_address)
-        company = OrgPs::Factory::Orgs.create_company(gui_number, name, office_address)
-        root = OrgPs::Factory::Nodes.create_root(company)
-        {
-          organization: company,
-          tree_node: root
-        }
-      end
+      class << self
+        def create_company_node(gui_number, name, office_address)
+          company = OrgPs::Factory::Orgs.create_company(gui_number, name, office_address)
+          root = OrgPs::Factory::Nodes.create_root(company)
+          {
+            organization: company,
+            tree_node: root
+          }
+        end
 
-      def add_org_node(parent_node, typing, name, code)
-        org = OrgPs::Factory::Orgs.create_org(typing, name, code)
-        node = OrgPs::Factory::Nodes.create_child(parent_node, org)
-        {
-          organization: org,
-          tree_node: node
-        }
+        def add_org_node(parent_node, typing, name, code)
+          org = OrgPs::Factory::Orgs.create_org(typing, name, code)
+          node = OrgPs::Factory::Nodes.create_child(parent_node, org)
+          {
+            organization: org,
+            tree_node: node
+          }
+        end
       end
     end
   end
