81688dc52e2b1b6fe7a1475c8ec1e6e997b3a955
wen <alejet.larvata@gmail.com>
2020-03-02 17:21:06 +0800

    feat 建立部門後加入主管與員工節點

--- lib/org_ps/service/org_nodes.rb
+++ lib/org_ps/service/org_nodes.rb
@@ -14,9 +14,20 @@ module OrgPs
         def add_org_node(parent_node, typing, name, code)
           org = OrgPs::Factory::Orgs.create_org(typing, name, code)
           node = OrgPs::Factory::Nodes.create_child(parent_node, org)
+          manager_org = OrgPs::Factory::Orgs.create_org('manager', "主管", "#{org.code}_manager")
+          manager_node = OrgPs::Factory::Nodes.create_child(node, manager_org)
+          member_org = OrgPs::Factory::Orgs.create_org('member', "員工", "#{org.code}_member")
+          member_node = OrgPs::Factory::Nodes.create_child(node, member_org)
+
           {
             organization: org,
-            tree_node: node
+            tree_node: {
+              node: node,
+              children: {
+                manager: manager_node,
+                member:  member_node
+              }
+            }
           }
         end
       end
