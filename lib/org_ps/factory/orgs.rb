80cf3c69a1789c9138cf873b2769b67b0be61cd8
wen <alejet.larvata@gmail.com>
2020-03-02 15:46:01 +0800

    feat Create 建立公司資料方法

--- lib/org_ps/factory/orgs.rb
+++ lib/org_ps/factory/orgs.rb
@@ -1,6 +1,14 @@
 module OrgPs
   module Factory
     class Orgs
+      def self.create_company(gui_number, name, office_address)
+        Org.com.enabled.create(
+          gui_number: gui_number,
+          abbrev_name: name,
+          full_name: name,
+          code: "company_#{gui_number}"
+        )
+      end
     end
   end
 end
