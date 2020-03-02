4b6edd6363dfaae592770cbed66e250f5368aa85
wen <alejet.larvata@gmail.com>
2020-03-02 16:46:44 +0800

    feat 組織節點類別由 host 專案指定

--- lib/org_ps.rb
+++ lib/org_ps.rb
@@ -2,9 +2,14 @@ module OrgPs
   require "org_ps/engine"
 
   mattr_accessor :org_class
+  mattr_accessor :tree_node_class
 
   def self.org_class
-    @org_class.constantize
+    @@org_class.constantize
+  end
+
+  def self.tree_node_class
+    @@tree_node_class.constantize
   end
 
   require 'org_ps/trunk/linkable'
--- lib/org_ps/factory/nodes.rb
+++ lib/org_ps/factory/nodes.rb
@@ -1,9 +1,8 @@
 module OrgPs
   module Factory
     class Nodes
-      def self.create_root(nodeable)
-        org = nodeable[:nodeable]
-        TreeNode.company.enabled.create(nodeable: org, org.gui_number, name: org.abbrev_name)
+      def self.create_root(org)
+        OrgPs.tree_node_class.com.enabled.create(nodeable: org, org.gui_number, name: org.abbrev_name)
       end
     end
   end
