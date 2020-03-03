d736398ba049b8e126d55c7bc9074ad8cd88eaad
wen <alejet.larvata@gmail.com>
2020-03-03 11:07:09 +0800

    feat 建立查詢公司與部門方法

--- lib/org_ps/finder/orgs.rb
+++ lib/org_ps/finder/orgs.rb
@@ -1,6 +1,23 @@
 module OrgPs
   module Finder
     class Orgs
+      attr_reader :company_node
+
+      def intitialze(gui_number)
+        @company_node = OrgPs.tree_node_class.roots.includes(:nodeable, :children).find_by_code(gui_number)
+      end
+
+      def child_org_node(typing, code)
+        org_node = company_code.children&.includes(:nodeable).send(typing)&.find_by_code(code)
+
+        {
+          organization: org_node&.nodeable,
+          tree_node: {
+            node: org_node,
+            parent: company_node
+          }
+        }
+      end
     end
   end
 end
